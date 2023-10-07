dd3a78f67a3b59bf7c262d232538b7027cfe7cc3
LoZimmero <fiorettigabriele@gmail.com>
2023-10-07 20:09:24 +0200

    Added venv

--- app/test/send_data.py
+++ app/test/send_data.py
@@ -1,6 +1,6 @@
 import requests
 
-filedata = open('../core/Recording 2023-10-07 17_51_02.wav', 'rb').read()
+filedata = open('../core/Recording 2023-10-07 19_15_40.mp3', 'rb').read()
 
 res = requests.post('http://localhost:9999/api/stt', data=filedata).json()
 
