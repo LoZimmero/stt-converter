b7d4c260bebf8d50045d1a1df283688a8d7e0226
LoZimmero <fiorettigabriele@gmail.com>
2023-10-08 21:08:11 +0200

    Edited base path in send_data.py

--- test/send_data.py
+++ test/send_data.py
@@ -1,6 +1,6 @@
 import requests
 
-filedata = open('../core/Prov.aac', 'rb').read()
+filedata = open('./Prov.aac', 'rb').read()
 
 res = requests.post('http://localhost:9999/api/stt', data=filedata).json()
 
